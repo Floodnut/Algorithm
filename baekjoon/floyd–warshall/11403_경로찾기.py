d9dda3599a35ae475e328939fc6446e54bdf0053
Floodnut <gsniper777@gmail.com>
2023-02-03 16:31:24 +0900

    [22.03.18] Python: 백준 11403 경로 찾기

--- baekjoon/floyd–warshall/11403_경로찾기.py
+++ baekjoon/floyd–warshall/11403_경로찾기.py
@@ -4,12 +4,10 @@ N = int(input())
 G = []
 node = [i for i in range(N)]
 
-for _ in range(N):
-    G.append(list(map(int, input().split())))
+for _ in range(N): G.append(list(map(int, input().split())))
 
 for k, x, y in core(node, repeat=3):
     if G[x][k] == 1 and G[k][y] == 1:
         G[x][y] = 1
 
-for i in G:
-    print(' '.join(map(str, i)))
+for i in G: print(' '.join(map(str, i)))
