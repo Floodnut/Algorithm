d980379e4c5179ec67796cb187bfdbf5b0c0ecb4
Floodnut <gsniper777@gmail.com>
2023-02-14 20:35:26 +0900

    [23.02.14] Java: 백준 9656 돌 게임2

---
+++ baekjoon/dp/9656_돌게임2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    
+    static enum Gamer{
+        나, CY
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(Gamer.values()[n % 2]);
+        
+        /*  Java 8
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            bw.write(Gamer.values()[n % 2].toString());
+
+            bw.flush();
+            bw.close();
+            br.close();
+        */
+    }
+}
