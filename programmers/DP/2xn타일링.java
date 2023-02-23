8e285861e365b40309f446440784376bfecb1f54
Floodnut <gsniper777@gmail.com>
2023-02-23 15:08:30 +0900

    [23.02.23] Java: 프로그래머스 12900 2xN타일링

---
+++ programmers/DP/2xn타일링.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        if(n <= 3)
+            return n;
+
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 3;
+
+        for(int i = 4 ; i <= n ; i++)
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 1000000007;
+
+        return arr[n];
+    }
+}
